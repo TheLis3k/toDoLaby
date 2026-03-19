833f7822ac05ff382b42f02bb7877ce495898e12
TheLis3k <ksawery2k19@gmail.com>
2026-03-19 18:55:16 +0100

    almost sito

---
+++ src/sito.php
@@ -0,0 +1,62 @@
+<?php
+
+function sito($n)
+{
+    $A = [];
+    for ($i = 0; $i <= $n; $i++) {
+        array_push($A, true);
+    }
+    $A[0] = $A[1] = false;
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($A[$i]) {
+            for ($j = $i * $i; $j <= $n; $j += $i) {
+                $A[$j] = false;
+            }
+        }
+    }
+    $result = [];
+    foreach ($A as $i => $value) {
+        if ($value) {
+            $result[] = $i;
+        }
+    }
+    return $result;
+}
+
+$array = sito(100);
+echo "Liczby pierwsze [1-100] bloki po 10:<br>[ ";
+foreach ($array as $i => $value) {
+    if ($i % 9 == 0 && $i != 0) {
+        echo $value;
+        echo " ]<br>[ ";
+    } elseif ($i == sizeof($array) - 1) {
+        echo $value . " ]<br>";
+    } else {
+        echo $value . ", ";
+    }
+}
+
+function density($a, $b)
+{
+    $mid = ($a + $b) / 2;
+    $density = ($b - $a) / log($mid);
+    return $density;
+}
+
+function countFirstNums($a, $b)
+{
+    $primesAll = sito($b);
+    $primes = [];
+    foreach ($primesAll as $value) {
+        if ($value >= $a) {
+            $primes[] = $value;
+        }
+    }
+    return count($primes);
+}
+echo "<br> Gęstość liczb pierwszych:<br>";
+echo "Przedział: [1-100]: " . countFirstNums(1, 100) . " | teoretycznie: ~" . density(1, 100) . "<br>";
+echo "Przedział: [101-200]: " . countFirstNums(101, 200) . " | teoretycznie: ~" . density(101, 200) . "<br>";
+echo "Przedział: [201-300]: " . countFirstNums(201, 300) . " | teoretycznie: ~" . density(201, 300) . "<br>";
+echo "Przedział: [301-400]: " . countFirstNums(301, 400) . " | teoretycznie: ~" . density(301, 400) . "<br>";
+echo "Przedział: [401-500]: " . countFirstNums(401, 500) . " | teoretycznie: ~" . density(401, 500) . "<br>";
